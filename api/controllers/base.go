baf0bdcb5065a3e1cb18f545c6517176f4afa203
Abdul Ghani Abbasi <schendazale@gmail.com>
2020-08-15 19:41:25 +0700

    fix db environment

--- api/controllers/base.go
+++ api/controllers/base.go
@@ -34,7 +34,7 @@ func (server *Server) Initialize(Dbdriver, DbUser, DbPassword, DbPort, DbHost, D
 		}
 	}
 	if Dbdriver == "postgres" {
-		DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", DbHost, DbPort, "abdulghn", "widi_group", "1234567890")
+		DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", DbHost, DbPort, DbUser, DbName, DbPassword)
 		server.DB, err = gorm.Open(Dbdriver, DBURL)
 		if err != nil {
 			fmt.Printf("Cannot connect to %s database", Dbdriver)
