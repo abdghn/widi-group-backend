6232ca57878de382a55ebe4bed931531aad4301e
Abdul Ghani Abbasi <schendazale@gmail.com>
2020-08-15 19:55:24 +0700

    comment auto migrate

--- api/server.go
+++ api/server.go
@@ -5,9 +5,9 @@ import (
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"product-order-be/api/controllers"
-	"product-order-be/api/seed"
+
+	"github.com/joho/godotenv"
 )
 
 var server = controllers.Server{}
@@ -24,7 +24,7 @@ func Run() {
 
 	server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
 
-	seed.Load(server.DB)
+	// seed.Load(server.DB)
 
 	server.Run(":8080")
 
