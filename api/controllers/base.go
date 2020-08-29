f2abece2ad6354df8caa89c76106201ce227641c
Abdul Ghani Abbasi <schendazale@gmail.com>
2020-08-29 17:54:46 +0700

    update url db

--- api/controllers/base.go
+++ api/controllers/base.go
@@ -23,25 +23,33 @@ func (server *Server) Initialize(Dbdriver, DbUser, DbPassword, DbPort, DbHost, D
 
 	var err error
 
-	if Dbdriver == "mysql" {
-		DBURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", DbUser, DbPassword, DbHost, DbPort, DbName)
-		server.DB, err = gorm.Open(Dbdriver, DBURL)
-		if err != nil {
-			fmt.Printf("Cannot connect to %s database", Dbdriver)
-			log.Fatal("This is the error:", err)
-		} else {
-			fmt.Printf("We are connected to the %s database", Dbdriver)
-		}
-	}
-	if Dbdriver == "postgres" {
-		DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", DbHost, DbPort, DbUser, DbName, DbPassword)
-		server.DB, err = gorm.Open(Dbdriver, DBURL)
-		if err != nil {
-			fmt.Printf("Cannot connect to %s database", Dbdriver)
-			log.Fatal("This is the error:", err)
-		} else {
-			fmt.Printf("We are connected to the %s database", Dbdriver)
-		}
+	// if Dbdriver == "mysql" {
+	// 	DBURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", DbUser, DbPassword, DbHost, DbPort, DbName)
+	// 	server.DB, err = gorm.Open(Dbdriver, DBURL)
+	// 	if err != nil {
+	// 		fmt.Printf("Cannot connect to %s database", Dbdriver)
+	// 		log.Fatal("This is the error:", err)
+	// 	} else {
+	// 		fmt.Printf("We are connected to the %s database", Dbdriver)
+	// 	}
+	// }
+	// if Dbdriver == "postgres" {
+	// 	DBURL := fmt.Sprintf("postgres://gtgmoyvbphgfiz:7017a8ec08bf52dc96a5ba951b1a8f9ae31e64f91f11fd96c2f90dda4847791c@ec2-54-156-121-142.compute-1.amazonaws.com:5432/d169utl5easvi", DbHost, DbPort, DbUser, DbName, DbPassword)
+	// 	server.DB, err = gorm.Open(Dbdriver, DBURL)
+	// 	if err != nil {
+	// 		fmt.Printf("Cannot connect to %s database", Dbdriver)
+	// 		log.Fatal("This is the error:", err)
+	// 	} else {
+	// 		fmt.Printf("We are connected to the %s database", Dbdriver)
+	// 	}
+	// }
+	DBURL := fmt.Sprintf("postgres://gtgmoyvbphgfiz:7017a8ec08bf52dc96a5ba951b1a8f9ae31e64f91f11fd96c2f90dda4847791c@ec2-54-156-121-142.compute-1.amazonaws.com:5432/d169utl5easvi")
+	server.DB, err = gorm.Open("postgres", DBURL)
+	if err != nil {
+		fmt.Printf("Cannot connect to %s database", Dbdriver)
+		log.Fatal("This is the error:", err)
+	} else {
+		fmt.Printf("We are connected to the %s database", Dbdriver)
 	}
 
 	server.DB.Debug().AutoMigrate(&models.User{}, &models.Order{}) //database migration
